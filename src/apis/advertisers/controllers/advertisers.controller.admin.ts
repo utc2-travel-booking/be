19fd89cd0f208af9d92ed47bfa09426987472f95
vyhcn3012 <vyhcn30122002@gmail.com>
2024-09-04 09:17:48 +0700

    []

--- src/apis/advertisers/controllers/advertisers.controller.admin.ts
+++ src/apis/advertisers/controllers/advertisers.controller.admin.ts
@@ -60,7 +60,6 @@ export class AdvertisersControllerAdmin {
         @Req() req: { user: UserPayload },
     ) {
         const { user } = req;
-        console.log('createAdvertiserDto', createAdvertiserDto);
         const result = await this.advertisersService.createOne(
             {
                 ...createAdvertiserDto,
@@ -80,7 +79,6 @@ export class AdvertisersControllerAdmin {
         @Req() req: { user: UserPayload },
     ) {
         const { user } = req;
-        console.log('createAdvertiserDto', updateAdvertiserDto);
 
         const result = await this.advertisersService.updateOneById(
             _id,
