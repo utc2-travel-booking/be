9fa9c1e0d500583696ff8377a184645ef36df95e
vyhcn3012 <vyhcn30122002@gmail.com>
2024-09-11 10:55:40 +0700

    [HOTFIX]

--- src/apis/auth/auth.service.ts
+++ src/apis/auth/auth.service.ts
@@ -10,14 +10,20 @@ export class AuthService {
     constructor(
         private readonly jwtService: JwtService,
         private readonly missionService: MissionService,
-        private readonly userService: UserService
-    ) { }
+        private readonly userService: UserService,
+    ) {}
 
     async login(user: UserPayload) {
         const tokens = await this.getTokens(user);
         const { telegramUserId } = await this.userService.getMe(user);
-        const mission = await this.missionService.updateMissionProcess([process.env.MISSION_ID_DAILY_LOGIN], telegramUserId.toString());
-        console.log(mission);
+
+        if (telegramUserId) {
+            await this.missionService.updateMissionProcess(
+                [process.env.MISSION_ID_DAILY_LOGIN],
+                telegramUserId.toString(),
+            );
+        }
+
         return tokens;
     }
 
